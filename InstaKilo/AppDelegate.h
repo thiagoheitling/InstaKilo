528473e4f901637c4eda109ad3304bed4443d633
Thiago Heitling <thiago.heitling@gmail.com>
2016-01-27 10:20:35 -0800

    first commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Thiago Heitling on 2016-01-27.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Thiago Heitling on 2016-01-27.
+//  Copyright © 2016 Thiago Heitling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
